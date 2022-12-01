436f89ca09ece22a7812fb8dd678b4fe16e05740
RiceBen <benjamin750715@gmail.com>
2022-12-01 13:43:02 +0800

    fix: coding error

--- AutofacExperiment.Access/Modules/DAModules.cs
+++ AutofacExperiment.Access/Modules/DAModules.cs
@@ -20,28 +20,23 @@ public class DAModules : Module
     /// </remarks>
     protected override void Load(ContainerBuilder builder)
     {
-        var assembly = Assembly.Load("AutofacExperiment.Access");
+        // builder.RegisterType<DeamonResourceRepository>()
+        //     .As<IDeamonResourceRepository>()
+        //     .InstancePerDependency();
 
-        builder.RegisterAssemblyTypes(assembly).OwnedByLifetimeScope();
+        // builder.RegisterType<DeamonResourceRepository>()
+        //     .As<IDeamonResourceRepository>()
+        //     .OwnedByLifetimeScope();
 
-        //// 目前 Deamon 的物件生命週期 policy
+        // 這個生命週期讓 MemoryLeakMethod02 不會有 memory leak 的問題
+        // 因為在同一個scope下取得的實體，將會是同一個實體。(新的scope就是新的實體)
         builder.RegisterType<DeamonResourceRepository>()
             .As<IDeamonResourceRepository>()
-            .InstancePerDependency();
+            .InstancePerLifetimeScope();
 
-        //builder.RegisterType<DeamonResourceRepository>()
-        //    .As<IDeamonResourceRepository>()
-        //    .OwnedByLifetimeScope();
-
-        //// 這個生命週期讓 MemoryLeakMethod02 不會有 memory leak 的問題
-        //// 因為在同一個scope下取得的實體，將會是同一個實體。(新的scope就是新的實體)
-        //builder.RegisterType<DeamonResourceRepository>()
-        //    .As<IDeamonResourceRepository>()
-        //    .InstancePerLifetimeScope();
-
-        ////用這個policy就不用擔心 memory leak 的問題
-        //builder.RegisterType<DeamonResourceRepository>()
-        //    .As<IDeamonResourceRepository>()
-        //    .SingleInstance();    
+        // 用這個policy就不用擔心 memory leak 的問題
+        // builder.RegisterType<DeamonResourceRepository>()
+        //     .As<IDeamonResourceRepository>()
+        //     .SingleInstance();
     }
 }
